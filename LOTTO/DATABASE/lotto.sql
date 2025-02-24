f3c3dc13ef9c9ac4950137b4a88ecd160d6ff2ea
Beatrice Dela Cruz <delacruzbeatrice10@gmail.com>
2025-02-24 18:28:41 +0800

    db

--- LOTTO/DATABASE/lotto.sql
+++ LOTTO/DATABASE/lotto.sql
@@ -16,6 +16,38 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `bets`
+--
+
+DROP TABLE IF EXISTS `bets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bets` (
+  `bet_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `numbers` varchar(60) NOT NULL,
+  `amount` decimal(10,2) DEFAULT '20.00',
+  `placed_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `round_id` int DEFAULT NULL,
+  `status` enum('Pending','Won','Lost') DEFAULT 'Pending',
+  PRIMARY KEY (`bet_id`),
+  KEY `user_id` (`user_id`),
+  KEY `round_id` (`round_id`),
+  CONSTRAINT `bets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  CONSTRAINT `bets_ibfk_2` FOREIGN KEY (`round_id`) REFERENCES `rounds` (`round_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bets`
+--
+
+LOCK TABLES `bets` WRITE;
+/*!40000 ALTER TABLE `bets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `rounds`
 --
 
@@ -73,4 +105,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-24 10:25:24
+-- Dump completed on 2025-02-24 18:27:50
