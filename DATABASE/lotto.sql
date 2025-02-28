9d51fdb29ae6762923a470d65aad90544ce57b26
Beatrice Dela Cruz <delacruzbeatrice10@gmail.com>
2025-02-28 18:53:29 +0800

    db

--- DATABASE/lotto.sql
+++ DATABASE/lotto.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.40, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.41, for Linux (x86_64)
 --
 -- Host: localhost    Database: lotto
 -- ------------------------------------------------------
--- Server version	8.0.40-0ubuntu0.24.04.1
+-- Server version	8.0.41-0ubuntu0.24.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,7 +26,7 @@ CREATE TABLE `bets` (
   `bet_id` int NOT NULL AUTO_INCREMENT,
   `user_id` int DEFAULT NULL,
   `numbers` varchar(60) NOT NULL,
-  `amount` decimal(10,2) DEFAULT '20.00',
+  `coins` int NOT NULL DEFAULT '20',
   `placed_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `round_id` int DEFAULT NULL,
   `status` enum('Pending','Won','Lost') DEFAULT 'Pending',
@@ -45,6 +45,32 @@ CREATE TABLE `bets` (
 LOCK TABLES `bets` WRITE;
 /*!40000 ALTER TABLE `bets` DISABLE KEYS */;
 /*!40000 ALTER TABLE `bets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prizes`
+--
+
+DROP TABLE IF EXISTS `prizes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prizes` (
+  `prize_id` int NOT NULL AUTO_INCREMENT,
+  `round_id` int NOT NULL,
+  `prize_amount` int NOT NULL DEFAULT '1000',
+  PRIMARY KEY (`prize_id`),
+  KEY `round_id` (`round_id`),
+  CONSTRAINT `round_id` FOREIGN KEY (`round_id`) REFERENCES `rounds` (`round_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prizes`
+--
+
+LOCK TABLES `prizes` WRITE;
+/*!40000 ALTER TABLE `prizes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prizes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -81,10 +107,14 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `user_id` int NOT NULL AUTO_INCREMENT,
   `user_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `birthdate` date NOT NULL,
-  `balance` decimal(10,2) DEFAULT '0.00',
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `contact_num` varchar(11) NOT NULL,
+  `coins` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,6 +123,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'hi','whuu','hihii','2000-01-01','123456789',0),(2,'heyboy','ima getcha','ima get you real good and i betcha','2000-01-01','123456789',0),(3,'bea','bea@gmail.com','f72df03dbd738c40a3561c8e7c5129c78aa22d0abcd9713988fcd79de53f4a42','2000-01-01','123456789',0),(5,'beatrice','bea1@gmail.com','78b7035cf7a238b5511002e8a50a1b153edad17829d16f2a641aed68dd61940a','2000-01-01','123456789',0),(6,'bea2','bea2@gmail.com','188ceb77dc8d0ada081927309a2a6237e2e83cb50e277efd0709364f7d308117','2000-01-01','123456789',0),(8,'bea3','bea3@gmail.com','188ceb77dc8d0ada081927309a2a6237e2e83cb50e277efd0709364f7d308117','2000-01-01','123456789',0),(9,'bea4','bea4@gmail.com','188ceb77dc8d0ada081927309a2a6237e2e83cb50e277efd0709364f7d308117','2000-01-01','123456789',0),(10,'bea5','bea5@gmail.com','188ceb77dc8d0ada081927309a2a6237e2e83cb50e277efd0709364f7d308117','2000-01-01','123456789',0),(11,'bea6','bea6@gmail.com','188ceb77dc8d0ada081927309a2a6237e2e83cb50e277efd0709364f7d308117','2000-01-01','123456789',0),(12,'bea7','bea7@gmail.com','188ceb77dc8d0ada081927309a2a6237e2e83cb50e277efd0709364f7d308117','2000-01-01','123456789',0),(13,'bea8','bea8@gmail.com','188ceb77dc8d0ada081927309a2a6237e2e83cb50e277efd0709364f7d308117','2000-01-01','123456789',0),(14,'bea9','bea9@gmail.com','188ceb77dc8d0ada081927309a2a6237e2e83cb50e277efd0709364f7d308117','2000-01-01','123456789',0),(15,'bea10','bea10@gmail.com','188ceb77dc8d0ada081927309a2a6237e2e83cb50e277efd0709364f7d308117','2000-01-01','123456789',0),(16,'bea11','bea11@gmail.com','188ceb77dc8d0ada081927309a2a6237e2e83cb50e277efd0709364f7d308117','2000-01-01','123456789',0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -105,4 +136,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-24 18:27:50
+-- Dump completed on 2025-02-28 18:51:53
--- backend/src/sockets/prizesSocket.js
+++ backend/src/sockets/prizesSocket.js
@@ -1,2 +1,42 @@
-import { Prizes } from "../models/prizesModel.js";
+import Prizes  from "../models/prizesModel.js";
 
+
+export class PrizeSocket{
+    constructor(io){
+        this.io = io;
+        this.prizeModel = new Prizes;
+        this. SocketEvents();
+    }
+
+    SocketEvents(){
+        this.io.on("connection",(socket) =>{
+            console.log("User at Prize page", socket.id);
+
+            socket.on("claimCoins", async(data)=> {
+                
+            });
+
+            socket.on("disconnect", () =>{
+                console.log("User disconnected", socket.id);
+            });
+        }); 
+    }
+
+    async claim_Coins(socket, data){
+        const user_id = socket.user_id;
+        console.log(user_id);
+
+        const {coins} = data;
+
+        try{
+            const result= await this.prizeModel.claim_coins(user_id, coins);
+
+            socket.emit("coinsClaimed
+        }
+    }
+
+
+
+
+
+}
